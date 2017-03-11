ed4aff304b9888af86753f6624841532be38b7e7
Louis Waweru <waweru@gmail.com>
2017-03-11 18:18:05 -0500

    Added Facebook authentication

--- Kieru/Startup.cs
+++ Kieru/Startup.cs
@@ -84,7 +84,11 @@ namespace Kieru
             app.UseIdentity();
 
             // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
-
+            app.UseFacebookAuthentication(new FacebookOptions()
+            {
+                AppId = Configuration["Authentication:Facebook:AppID"],
+                AppSecret = Configuration["Authentication:Facebook:AppSecret"]
+            });
 
             app.UseMvc(routes =>
             {
