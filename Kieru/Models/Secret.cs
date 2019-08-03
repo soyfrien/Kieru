b6b0fe00263b6d3ee56402216331e12315849676
Louis A. Waweru <l.waweru@columbia.edu>
2019-08-03 02:10:47 -0400

    CVE-2017-0249 High severity CVE-2017-0247 Moderate severity CVE-2017-0248 Moderate severity Kieru.csproj update suggested: Microsoft.AspNetCore.Mvc ~> 1.1.3

--- Kieru/Models/Secret.cs
+++ Kieru/Models/Secret.cs
@@ -1,10 +1,6 @@
-﻿using Kieru.Utilities;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Kieru.Models
 {
